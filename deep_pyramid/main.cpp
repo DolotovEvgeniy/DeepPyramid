8ebe0c570670301b2b471e4b9a15c1feb969b80f
Dolotov Evgeniy <dolotov.evgeniy@gmail.com>
2015-10-18 15:06:15 +0300

    addRootFilter in main()

--- deep_pyramid/main.cpp
+++ deep_pyramid/main.cpp
@@ -17,15 +17,17 @@ int main(int argc, char *argv[])
 
     string alexnet_model_file=argv[1];
     string alexnet_trained_file=argv[2];
-
+    string svm_trained_file=argv[3];
     DeepPyramid pyramid(7,alexnet_model_file, alexnet_trained_file);
-    string image_file=argv[3];
+    string image_file=argv[4];
+
+    CvSVM classifier;
+    classifier.load(svm_trained_file.c_str());
+    pyramid.addRootFilter(Size(7,11),&classifier);
 
     image=imread(image_file, CV_LOAD_IMAGE_COLOR);
+
     pyramid.detect(image);
-    //namedWindow("Image", WINDOW_AUTOSIZE);
-    //imshow("Image",image);
-    //waitKey(0);
-    //pyramid.calculate(image);
+
     return 0;
 }

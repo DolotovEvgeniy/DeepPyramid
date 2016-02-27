0283854bcb5bdf36e1a2a6f298168acb01e2b6a0
Dolotov Evgeniy <dolotov.evgeniy@gmail.com>
2016-02-27 10:22:25 +0300

    delete other models

--- ddfd_alexnet/conv_train.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-GLOG_logtostderr=1 ./build/tools/caffe train --solver=examples/ddfd_alexnet/conv_solver.prototxt --weights=models/bvlc_alexnet/bvlc_alexnet.caffemodel --gpu 0 2>&1 | tee examples/ddfd_alexnet/log
